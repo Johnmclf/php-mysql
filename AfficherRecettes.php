edc00a5b152ae0d339cc03d02f61c464707c3e45
Johnmclf <150351096+Johnmclf@users.noreply.github.com>
2024-09-06 11:05:36 +0200

    Les tableaux associatifs

--- AfficherRecettes.php
+++ AfficherRecettes.php
@@ -1,8 +1,19 @@
 <?php
 // Déclaration du tableau des recettes
+// $recipes = [
+// ['Cassoulet','[...]','mickael.andrieu@exemple.com',true,],
+// ['Couscous','[...]','mickael.andrieu@exemple.com',false,],
+// ];
+?>
+
+<?php
 $recipes = [
-['Cassoulet','[...]','mickael.andrieu@exemple.com',true,],
-['Couscous','[...]','mickael.andrieu@exemple.com',false,],
+['title' => 'Cassoulet','recipe' => 'des flageoets','author' => 'mickael.andrieu@exemple.com',
+'is_enabled' => true,],
+['title' => 'Couscous','recipe' => '','author' => 'mickael.andrieu@exemple.com',
+'is_enabled' => false,],
+['title' => 'Escalope milanaise','recipe' => 'prenez une belle escalope','author' => 'mathieu.nebra@exemple.com',
+'is_enabled' => true,]
 ];
 ?>
 
@@ -10,14 +21,19 @@ $recipes = [
 <html>
 <head>
 <title>Affichage des recettes</title>
+<link rel="stylesheet" href="style.css">
 </head>
 <body>
-<ul>
-<?php for ($lines = 0; $lines <= 1; $lines++): ?>
-<li>
-<?php echo $recipes[$lines][0] . ' (' . $recipes[$lines][2] . ')';?>
-</li>
+    <h1>Affichage des recettes</h1>
+<div>
+<?php for ($lines = 0; $lines <= 1; $lines++): ?> 
+<?php
+foreach($recipes as $recipe) {
+    if($recipe['is_enabled']==true){
+        echo '<h2>'.$recipe['title'] . '</h2> Etape 1 : ' . $recipe['recipe'] . '<br>' . $recipe['author'] . '<br>';
+    }
+}?>
 <?php endfor; ?>
-</ul>
+</div>
 </body>
 </html>
